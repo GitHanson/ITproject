402c4e6dd365fe532551a1ca0e4be8599db8f3ed
Xudong Zhang <x.zhang177@student.unimelb.edu.au>
2019-09-07 17:15:43 +1000

    join family page (layout only)

---
+++ FamilyAPP2/app/src/main/java/com/example/familyapp2/JoinFamilyActivity.java
@@ -0,0 +1,14 @@
+package com.example.familyapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinFamilyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_family);
+    }
+}
