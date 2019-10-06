fa2658b6449485c77cc8255ae58bb0cb1f54e746
Hao Liu <h.liu63@student.unimelb.edu.au>
2019-10-06 15:27:09 +1100

    diaplay picture frams in photo video and document pages

--- FamilyAPP2/app/src/main/java/com/example/familyapp2/PersonalArtifactActivity.java
+++ FamilyAPP2/app/src/main/java/com/example/familyapp2/PersonalArtifactActivity.java
@@ -2,7 +2,10 @@ package com.example.familyapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,6 +35,28 @@ public class PersonalArtifactActivity extends AppCompatActivity {
 
         //list of artifacts
         ListView listitem = PersonalArtifactActivity.this.findViewById(R.id.list_item);
+        listitem.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 5;
+
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                return LayoutInflater.from(PersonalArtifactActivity.this).inflate(R.layout.personal_artifact_list,null);
+            }
+        });
 
     }
 
