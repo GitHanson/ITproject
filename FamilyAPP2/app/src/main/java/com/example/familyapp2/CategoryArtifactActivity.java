9063bfc4a7934fd65f73681168b57e0e81bc41c7
Xudong Zhang <xudongz1@student.unimelb.edu.au>
2019-10-09 19:42:24 +1100

    image shows in category, no filter added

--- FamilyAPP2/app/src/main/java/com/example/familyapp2/CategoryArtifactActivity.java
+++ FamilyAPP2/app/src/main/java/com/example/familyapp2/CategoryArtifactActivity.java
@@ -20,10 +20,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.familyapp2.fragment.CategoryFragment;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -31,57 +35,33 @@ import java.util.List;
 
 public class CategoryArtifactActivity extends AppCompatActivity {
 
-//    FirebaseDatabase database;
-//    DatabaseReference userRef;
-//    DatabaseReference artifactRef;
-//
-//    private RecyclerView mRecyclerView;
-//    private ImageAdapterCategory mAdapter;
-//    private List<Artifact> mArtifacts;
+    RecyclerView mRecyclerView;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mRef;
+    String cateName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_artifact);
 
-//        mRecyclerView = findViewById(R.id.category_recycler);
-//        mRecyclerView.setHasFixedSize(true);
-//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//
-//        mArtifacts = new ArrayList<>();
-//
-//        // init firebase
-//        database = FirebaseDatabase.getInstance();
-//        userRef = database.getReference("Users");
-//        artifactRef = database.getReference("Artifacts");
-//
-//        artifactRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                for (DataSnapshot postSnapshot :  dataSnapshot.getChildren()) {
-//                    Artifact artifact = postSnapshot.getValue(Artifact.class);
-//                    artifact.setKey(postSnapshot.getKey());
-//                    mArtifacts.add(artifact);
-//                }
-//
-//                mAdapter = new ImageAdapterCategory(CategoryArtifactActivity.this, mArtifacts);
-//
-//                mRecyclerView.setAdapter(mAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                Toast.makeText(CategoryArtifactActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
 
+        //recycler view
+        mRecyclerView = findViewById(R.id.category_recycler);
+        mRecyclerView.setHasFixedSize(true);
 
+        //set layout as Linear
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //send query to Firebase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mRef = mFirebaseDatabase.getReference("Artifacts");
 
 
 
         // different category name
         TextView cateNameTextView = findViewById(R.id.cateName);
-        String cateName = CategoryFragment.getCategoryName();
+        cateName = CategoryFragment.getCategoryName();
         cateNameTextView.setText(cateName);
 
         // implement back button
@@ -93,32 +73,62 @@ public class CategoryArtifactActivity extends AppCompatActivity {
             }
         });
 
-        //list of artifacts
-//        ListView artifactListView = CategoryArtifactActivity.this.findViewById(R.id.cate_artifact_list);
-//        artifactListView.setAdapter(new BaseAdapter() {
-//            @Override
-//            public int getCount() {
-//                return 5;
-//            }  //how many artifact in this category
-//
-//            @Override
-//            public Object getItem(int i) {
-//                return null;
-//            }
-//
-//            @Override
-//            public long getItemId(int i) {
-//                return 0;
-//            }
-//
-//            @Override
-//            public View getView(int i, View view, ViewGroup viewGroup) {
-//                return LayoutInflater.from(CategoryArtifactActivity.this).inflate(R.layout.list_item_artifact, null);
-//            }
-//        });
     }
 
+    //load data into recycler view onStart
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Artifacts> options =
+                new FirebaseRecyclerOptions.Builder<Artifacts>()
+                        .setQuery(mRef, Artifacts.class)
+                        .build();
+
+//        Query query = mRef.orderByChild("family_category_privacy").equalTo("03_"+cateName.toLowerCase()+"_1");
+//        query.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.exists()) {
+//
+//                    for (DataSnapshot issue : dataSnapshot.getChildren()) {
+//                        // do with your result
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+        FirebaseRecyclerAdapter<Artifacts, CategoryViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Artifacts, CategoryViewHolder>(options) {
+
+                    @NonNull
+                    @Override
+                    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext())
+                                .inflate(R.layout.list_item_artifact, parent, false);
+
+                        return new CategoryViewHolder(view);
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull CategoryViewHolder holder, int position, @NonNull Artifacts model) {
+                        //if (model.getFamily_category_privacy().equals("03_"+cateName.toLowerCase()+"_1")) {
+                            holder.setDetails(getApplicationContext(), model.getArtifactUrl());
+                        //}
+                    }
+
+                };
+
+        //set adapter to recycler view
+        firebaseRecyclerAdapter.startListening();
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
 
     // back to previous activity
     @Override
