ac85b0d3d3d5e603b9b654f38a10484abad8817c
Xudong Zhang <xudongz1@student.unimelb.edu.au>
2019-10-10 21:09:42 +1100

    home page complete

---
+++ FamilyAPP2/app/src/main/java/com/example/familyapp2/HomeViewHolder.java
@@ -0,0 +1,35 @@
+package com.example.familyapp2;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+public class HomeViewHolder extends RecyclerView.ViewHolder {
+    View mView;
+
+    public HomeViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mView = itemView;
+    }
+
+    //set details to item in recycler view
+    public void setDetails(Context ctx, String image_artifact, String image_icon, String userName, String description) {
+        //view
+        ImageView mImageViewArtifact = mView.findViewById(R.id.home_image);
+        ImageView mImageViewIcon = mView.findViewById(R.id.home_icon);
+        TextView mTextView = mView.findViewById(R.id.home_description);
+        TextView mUserName = mView.findViewById(R.id.home_user_name);
+        //set data to views
+        Picasso.get().load(image_artifact).into(mImageViewArtifact);
+        Picasso.get().load(image_icon).into(mImageViewIcon);
+        mTextView.setText(description);
+        mUserName.setText(userName);
+}
+}
